4a7a2c33381714600553dca153b0f9bd5fd0bf0f
Crhistian Ovalle <desarrolloextremo@gmail.com>
2024-05-09 19:51:38 -0500

    Update 6_tuplas.py

--- 6_tuplas.py
+++ 6_tuplas.py
@@ -8,25 +8,27 @@ Métodos principales:
 index(valor): Devuelve el índice de la primera aparición del valor especificado.
 count(valor): Devuelve el número de veces que aparece un valor en la tupla.
 '''
+from funciones_captura import limpiar_pantalla
+
+limpiar_pantalla()
 
 #definir una tupla para los días de la semana
-dias_semana=("LUNES","MARTES","MIERCOLES","JUEVES","VIERNES","SABADO","DOMINGO")
+dias_semana=("LUNES","MARTES","MIERCOLES","JUEVES","VIERNES","SABADO","DOMINGO","SABADO")
 
 #imprimir la tupla
 print(f"dias de la semana {dias_semana}")
 
 #cantidad de elementos de la tupla
 #El método index devuelve el índice de la primera aparición de un valor en la tupla.
-indice = dias_semana.count("LUNES")
-print(f"El índice de la primera aparición del valor LUNES es: {indice}")
+indice = dias_semana.index("SABADO")
+print(f"El índice de la primera aparición del valor SABADO es: {indice}")
 
 # Usando el método count para contar cuántas veces aparece el valor 3 en la tupla
-conteo = dias_semana.count("MIERCOLES")
-print(f"El valor MIERCOLES aparece {conteo} veces en la tupla") 
+conteo = dias_semana.count("SABADO")
+print(f"El valor SABADO aparece {conteo} veces en la tupla")
 
 #Acceder al valor en una posicion de la tupla
 print(f"El contenido de la tupla en la posición 1 es {dias_semana[1]}")
-
 
 print(f"Imprimir el contenido de la tupla")
 #recorrer una tupla
@@ -35,7 +37,7 @@ for elemento in dias_semana:
 
 #Slicing (rebanado):
 #Puedes obtener subconjuntos de elementos de una tupla utilizando la notación de slicing [inicio:fin].
-subtupla = dias_semana[0:2]  # Obteniendo una subtupla desde el elemento 0 hasta el segundo elemento
+subtupla = dias_semana[3:5]  # Obteniendo una subtupla desde el elemento 3 hasta el segundo elemento 5
 print("Subtupla:", subtupla)
 
 #Longitud de la tupla usando len(nombre tupla)
@@ -47,4 +49,3 @@ print(f"Cantidad de elementos de la tupla: {len(dias_semana)}")
 #puedes crear una lista a partir de una tupla (aunque veremos las listas mas tarde)
 lista_dias_semana = list(dias_semana)
 print("Lista creada a partir de la tupla:", lista_dias_semana) 
-
